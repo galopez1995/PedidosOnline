0e1ee001b94cfec63df177069cac505edcaac887
galopez1995 <galopez1995@gmail.com>
2016-02-05 16:49:13 -0500

    05022016

--- js/app.js
+++ js/app.js
@@ -37,18 +37,18 @@ app_angular.config(['$routeProvider',//'$locationProvider',
 
 //CONTROLADOR DE GENERAL
 app_angular.controller('appController', function ($scope, $routeParams, Factory) {
-    console.log($routeParams);
-    $scope.templateUrl = 'view/' + $routeParams.modulo + '/' + $routeParams.url + '.html';
+    if (window.localStorage.getItem("user") == null || window.localStorage.getItem("user")==undefined) {
+        console.log($routeParams);
+        //$scope.templateUrl = 'login.html';
+        $location.path("/login.html");
+    }
 
+    if ($routeParams.url == undefined) {
+        //===== Sidebar Search (Demo Only) =====//
+        angular.element(document).ready(function () {
+            //try {
 
-    //===== Sidebar Search (Demo Only) =====//
-    angular.element(document).ready(function () {
-        try {
-            /*$scope.modulo = $routeParams.modulo;
-             $scope.url = $routeParams.url;*/
-
-
-            angular.element('#titulo').html($routeParams.url);
+            //angular.element('#titulo').html($routeParams.url);
 
 
             App.init(); // Init layout and core plugins
@@ -115,49 +115,16 @@ app_angular.controller('appController', function ($scope, $routeParams, Factory)
             setTimeout(function () {
                 angular.element('#sidebar .notifications.demo-slide-in > li:eq(0)').slideDown(500);
             }, 7000);
-
-
-            // Sample Data
-            var d1 = [[1262304000000, 0], [1264982400000, 500], [1267401600000, 700], [1270080000000, 1300], [1272672000000, 2600], [1275350400000, 1300], [1277942400000, 1700], [1280620800000, 1300], [1283299200000, 1500], [1285891200000, 2000], [1288569600000, 1500], [1291161600000, 1200]];
-
-            var data1 = [
-                {label: "Total Ventas", data: d1, color: App.getLayoutColorCode('blue')}
-            ];
-
-            $.plot("#chart_filled_blue", data1, $.extend(true, {}, Plugins.getFlotDefaults(), {
-                xaxis: {
-                    min: (new Date(2009, 12, 1)).getTime(),
-                    max: (new Date(2010, 11, 2)).getTime(),
-                    mode: "time",
-                    tickSize: [1, "month"],
-                    monthNames: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
-                    tickLength: 0
-                },
-                series: {
-                    lines: {
-                        fill: true,
-                        lineWidth: 1.5
-                    },
-                    points: {
-                        show: true,
-                        radius: 2.5,
-                        lineWidth: 1.1
-                    },
-                    grow: {active: true, growings: [{stepMode: "maximum"}]}
-                },
-                grid: {
-                    hoverable: true,
-                    clickable: true
-                },
-                tooltip: true,
-                tooltipOpts: {
-                    content: '%s: %y'
-                }
-            }));
-        }
-        catch (excepcion) {
-        }
-    });
+            /*    }
+             catch (excepcion) {
+             debugger;
+             }*/
+        });
+    }
+    else {
+        console.log($routeParams);
+        $scope.templateUrl = 'view/' + $routeParams.modulo + '/' + $routeParams.url + '.html';
+    }
 
 });
 
@@ -177,6 +144,19 @@ app_angular.controller('menuController', function ($scope, Factory) {
             }]
         }
     ];
+});
+
+//CONTROLADOR DEL LOGIN
+app_angular.controller('loginController', function ($scope, Factory) {
+    angular.element(document).ready(function () {
+        "use strict";
+        Login.init(); // Init login JavaScript
+    });
+
+    $scope.Login=function(){
+        window.localStorage.setItem("user", "user:xxx;pass:xxxxxx;");
+        debugger;
+    }
 });
 
 //CONTROLADOR DE PANTALLA DE CALENDARIO
