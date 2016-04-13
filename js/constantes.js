69286bf2c400e67a0a8a91a74487271bbefe15d4
galopez1995 <galopez1995@gmail.com>
2016-04-13 00:37:17 -0500

    13-04-15

---
+++ js/constantes.js
@@ -0,0 +1,25 @@
+var ENTIDAD_PEDIDOS='PEDIDOS';
+var ENTIDAD_PEDIDOS_DETALLE='PEDIDOS_DETALLE';
+var ENTIDAD_TERCEROS='TERCEROS';
+var ENTIDAD_SUCURSALES='SUCURSALES';
+var ENTIDAD_PUNTOS_ENVIO='PUNTOS_ENVIO';
+
+var STEP_SINCRONIZACION=
+    [
+        //                    'MAESTROS',
+        //                    'MODULOS_CONFIG',
+        //                    'OPCIONES_MENU',
+        //                    'PARAMETROS',
+        //                    'METACLASS',
+        //                    'LOCALIZACION',
+        //                    'ITEMS',
+        //                    'ITEMS_PRECIOS',
+        ENTIDAD_PEDIDOS,
+        ENTIDAD_PEDIDOS_DETALLE,
+        ENTIDAD_TERCEROS//,
+        //ENTIDAD_SUCURSALES,
+        //ENTIDAD_PUNTOS_ENVIO
+];
+
+var DATABASE='PedidosOnlineaass';
+var DATOS_ENTIDADES_SINCRONIZACION = [];
